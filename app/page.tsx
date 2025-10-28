c92586b16366ea92da6534980ff235ed56e08ae9
Mintzy-V1 <mintzy01.ai@gmail.com>
2025-10-28 12:57:11 +0530

    added the correct import of useuser in the app.page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,43 @@
-"use client"
+"use client";
 
-import React from 'react'
-import UploadForm from './components/UploadForm'
-import { ReactQueryProvider } from './components/ReactQueryProvider'
-import Herosection from './components/HeroSection'
-import { useUser } from '@clerk/nextjs'
-import axios from 'axios'
+import React from "react";
+import { useUser } from "@clerk/nextjs";
+import axios from "axios";
 
+import { ReactQueryProvider } from "./components/ReactQueryProvider";
+import UploadForm from "./components/UploadForm";
+import Herosection from "./components/HeroSection";
 
-const page = () => {
+const Page = () => {
+  const { isSignedIn } = useUser();
 
-  const { isSignedIn} = useUser();
-
-  const handlepost = async () =>{
+  const handlePost = async () => {
     const res = await axios.post(`/api/aiCoach`);
     console.log(res);
-  }
+  };
 
-  // const handlecreatesession = async ()=>{
-  //   const res = await axios.post(`/api/interview`,
-  //     {
-        
-  //     }
-  //   )
-  // }
-
-
-  const handleSaveUser = async() =>{
+  const handleSaveUser = async () => {
     try {
-      console.log("inside the handle saveruser ")
-      const res = await axios.post(`/api/user`)
-      console.log(res  , "res data ")
+      console.log("inside handleSaveUser");
+      const res = await axios.post(`/api/user`);
+      console.log(res, "res data");
     } catch (error) {
-      console.error(error , "error in saving the user")
-      console.log("error in saving the user");
+      console.error("Error in saving the user", error);
     }
-  }
-  if(isSignedIn){
+  };
+
+  if (isSignedIn) {
     handleSaveUser();
   }
-  return (
-    <div className=''>
-      <ReactQueryProvider>
-      <Herosection/>
-      <button onClick={()=>{handlepost()}}>aicoach</button>
-      {/* <button onClick={()=>{handlecreatesession()}}>create session</button> */}
-      </ReactQueryProvider>
-      
-    </div>
-  )
-}
 
-export default page
+  return (
+    <div>
+      <ReactQueryProvider>
+        <Herosection />
+        <button onClick={handlePost}>AI Coach</button>
+      </ReactQueryProvider>
+    </div>
+  );
+};
+
+export default Page;
